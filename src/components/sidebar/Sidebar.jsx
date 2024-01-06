c2d6b0a804884801f95bf11a5e0da1f39bcd05a8
Appalaraju.Siripalli <Appalaraju.Siripalli@jbhunt.com>
2024-01-06 23:01:35 +0530

    Home layout sidebar header updated

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SidebarHeader from "./header/SidebarHeader";
+
+export default function Sidebar() {
+  return (
+    <div className="flex0030 max-w-[30%] h-full select-none">
+      {/*Sidebar Header*/}
+      <SidebarHeader />
+    </div>
+  );
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Sidebar } from "../components/sidebar";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center overflow-hidden">
+        {/*container*/}
+        <div className="container h-screen flex py-[19px]">
+          {/*Sidebar*/}
+          <Sidebar />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
